e188e36889028d5f47fa6954af44d32aa50fb449
Salavat Nigmatullin <salavat@nigmatullin.net>
2024-08-17 22:13:14 +0500

    Fixed problems. Added IP/Mask validator

--- 2024-08-16_ip-calc/main.go
+++ 2024-08-16_ip-calc/main.go
@@ -2,38 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 	// "time"
 )
 
-var net_host = "188.234.148.235"
-var net_mask = 17
-var number = 255
+var net_host = "192.168.100.155"
+var net_mask = 21
+var myBinaryIP []string
 
 func main() {
-	// fmt.Print("Enter IP address:  ")
-	// _, _ = fmt.Scanln(&net_host)
-	// fmt.Print("Enter mask length: ")
-	// _, _ = fmt.Scanln(&net_mask)
+	myBinaryIP := getBinaryIP(net_host) // [11111111 11111111 11111111 11111010]
+	validatorNetwork(myBinaryIP, net_mask)
 
-	result := getBinaryIP(net_host)
-	// fmt.Printf("result1:	%s\n", result)
-	fmt.Printf("Address:	%s\n", net_host)
-	// fmt.Printf("result2:	%s\n", result)
-	fmt.Printf("Network: 	%s\n", getNetwork(result, net_mask))
-	// fmt.Printf("result3:	%s\n", result)
-	fmt.Printf("HostMin:	%s\n", getHostMin(result, net_mask))
-	// fmt.Printf("result3:	%s\n", result)
-	// fmt.Printf("Last IP Address: %s\n", lastIP.String())
-	fmt.Printf("Broadcast:	%s\n", getBroadcast(result, net_mask))
-
-}
-
-// In-Progress
-// Будет получать IP адрес и маску сети на проверку
-func validatorNetwork() string {
-	return ""
+	fmt.Printf("Address: 	%s\n", net_host)
+	fmt.Printf("Network: 	%s\n", getNetwork(myBinaryIP, net_mask))
+	fmt.Printf("HostMin:	%s\n", getHostMin(myBinaryIP, net_mask))
+	fmt.Printf("HostMax:	%s\n", getHostMax(myBinaryIP, net_mask))
+	fmt.Printf("Broadcast:	%s\n", getBroadcast(myBinaryIP, net_mask))
 }
 
 // getBinaryIP принимает строку IP-адреса в формате "192.168.20.1"
@@ -65,121 +52,179 @@ func getBinaryIP(myNetwork string) []string {
 // Например, принимает [11000000 10101000 11111111 11111111] и маску 15.
 // Возвращает "192.254.0.0"
 func getNetwork(binaryNet []string, netmask int) string {
+	var networkAddress []string
+
 	binaryNetwork := make([]string, 4)
 	copy(binaryNetwork, binaryNet)
 
-	// binaryNetwork := binaryNet
-	var data []string
-
-	if netmask >= 24 {
-		networkOctet := 8 - (32 - netmask)
+	if netmask < 8 {
+		octet := binaryNetwork[1]
+		octet = octet[:netmask] + strings.Repeat("0", 8-netmask)
+		copy(binaryNetwork[0:], []string{octet, "00000000", "00000000", "00000000"})
+	} else if netmask >= 8 && netmask < 16 {
+		networkOctet := -8 + netmask
+		octet := binaryNetwork[1]
+		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet)
+		copy(binaryNetwork[1:], []string{octet, "00000000", "00000000"})
+	} else if netmask >= 16 && netmask < 24 {
+		networkOctet := -16 + netmask
+		octet := binaryNetwork[2]
+		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet)
+		copy(binaryNetwork[2:], []string{octet, "00000000"})
+	} else if netmask >= 24 {
+		networkOctet := -24 + netmask
 		octet := binaryNetwork[3]
 		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet)
-		binaryNetwork[3] = octet
-
-	} else if netmask >= 16 && netmask < 24 {
-		networkOctet := 8 - (32 - netmask - 8)
-		octet := binaryNetwork[2]
-		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet)
-		binaryNetwork[3] = "00000000"
-		binaryNetwork[2] = octet
-
-	} else if netmask >= 8 && netmask < 16 {
-		networkOctet := 8 - (32 - netmask - 8 - 8)
-		octet := binaryNetwork[2]
-		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet)
-		binaryNetwork[3] = "00000000"
-		binaryNetwork[2] = "00000000"
-		binaryNetwork[1] = octet
-	}
+		copy(binaryNetwork[3:], []string{octet})
+	} // binaryNetwork = [00001010 11111111 11100000 00000000]
 
 	for _, octet := range binaryNetwork {
-		ip_octet, _ := strconv.ParseInt(octet, 2, 64)
-		str := fmt.Sprintf("%d", ip_octet)
-		data = append(data, str)
-	}
+		octetIP, _ := strconv.ParseInt(octet, 2, 64)
+		networkAddress = append(networkAddress, fmt.Sprintf("%d", octetIP))
+	} // networkAddress = [10 255 224 0]
 
-	network := strings.Join(data, ".")
+	network := strings.Join(networkAddress, ".")
 	mask := strconv.Itoa(netmask)
-	return network + "/" + mask
-}
 
-func getBroadcast(binaryNet []string, netmask int) string {
-	binaryNetwork := make([]string, 4)
-	copy(binaryNetwork, binaryNet)
-	var data []string
-
-	if netmask >= 24 {
-		networkOctet := 8 - (32 - netmask)
-		octet := binaryNetwork[3]
-		octet = octet[:networkOctet] + strings.Repeat("1", 8-networkOctet)
-		binaryNetwork[3] = octet
-
-	} else if netmask >= 16 && netmask < 24 {
-		networkOctet := 8 - (32 - netmask - 8)
-		octet := binaryNetwork[2]
-		octet = octet[:networkOctet] + strings.Repeat("1", 8-networkOctet)
-		binaryNetwork[3] = "11111111"
-		binaryNetwork[2] = octet
-
-	} else if netmask >= 8 && netmask < 16 {
-		networkOctet := 8 - (32 - netmask - 8 - 8)
-		octet := binaryNetwork[2]
-		octet = octet[:networkOctet] + strings.Repeat("1", 8-networkOctet)
-		binaryNetwork[3] = "11111111"
-		binaryNetwork[2] = "11111111"
-		binaryNetwork[1] = octet
-	}
-
-	for _, octet := range binaryNetwork {
-		ip_octet, _ := strconv.ParseInt(octet, 2, 64)
-		str := fmt.Sprintf("%d", ip_octet)
-		data = append(data, str)
-	}
-	// fmt.Println("getBroadcast:  ", binaryNetwork)
-	network := strings.Join(data, ".")
-	return network
+	return network + "/" + mask // 10.255.224.0/19
 }
 
 func getHostMin(binaryNet []string, netmask int) string {
+	var networkAddress []string
+
 	binaryNetwork := make([]string, 4)
 	copy(binaryNetwork, binaryNet)
-	var data []string
 
-	if netmask >= 24 {
-		networkOctet := 8 - (32 - netmask)
-		octet := binaryNetwork[3]
-		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet)
-		// fmt.Println("getHostMin, octet1: ", octet)
-		octet = octet[:7] + "1"
-		// fmt.Println("getHostMin, octet2: ", octet)
-
-		binaryNetwork[3] = octet
-
-	} else if netmask >= 16 && netmask < 24 {
-		networkOctet := 8 - (32 - netmask - 8)
-		octet := binaryNetwork[2]
-		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet)
-		binaryNetwork[3] = "00000001"
-		binaryNetwork[2] = octet
-
+	if netmask < 8 {
+		octet := binaryNetwork[1]
+		octet = octet[:netmask] + strings.Repeat("0", 8-netmask)
+		copy(binaryNetwork[0:], []string{octet, "00000000", "00000000", "00000001"})
 	} else if netmask >= 8 && netmask < 16 {
-		networkOctet := 8 - (32 - netmask - 8 - 8)
-		octet := binaryNetwork[2]
-		fmt.Println("Octet1: ", octet)
+		networkOctet := -8 + netmask
+		octet := binaryNetwork[1]
 		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet)
-		fmt.Println("Octet2: ", octet)
-		binaryNetwork[3] = "00000001"
-		binaryNetwork[2] = "00000000"
-		binaryNetwork[1] = octet
+		copy(binaryNetwork[1:], []string{octet, "00000000", "00000001"})
+	} else if netmask >= 16 && netmask < 24 {
+		networkOctet := -16 + netmask
+		octet := binaryNetwork[2]
+		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet)
+		copy(binaryNetwork[2:], []string{octet, "00000001"})
+	} else if netmask >= 24 {
+		networkOctet := -24 + netmask
+		octet := binaryNetwork[3]
+		octet = octet[:networkOctet] + strings.Repeat("0", 8-networkOctet-1) + "1"
+		copy(binaryNetwork[3:], []string{octet})
+	} // binaryNetwork = [00001010 11111111 11100000 00000000]
+
+	for _, octet := range binaryNetwork {
+		octetIP, _ := strconv.ParseInt(octet, 2, 64)
+		networkAddress = append(networkAddress, fmt.Sprintf("%d", octetIP))
+	} // networkAddress = [10 255 224 0]
+
+	minHost := strings.Join(networkAddress, ".")
+
+	return minHost // 10.255.255.241
+}
+
+func getHostMax(binaryNet []string, netmask int) string {
+	var networkAddress []string
+
+	binaryNetwork := make([]string, 4)
+	copy(binaryNetwork, binaryNet)
+
+	if netmask < 8 {
+		octet := binaryNetwork[1]
+		octet = octet[:netmask] + strings.Repeat("1", 8-netmask)
+		copy(binaryNetwork[0:], []string{octet, "11111111", "11111111", "11111110"})
+	} else if netmask >= 8 && netmask < 16 {
+		networkOctet := -8 + netmask
+		octet := binaryNetwork[1]
+		octet = octet[:networkOctet] + strings.Repeat("1", 8-networkOctet)
+		copy(binaryNetwork[1:], []string{octet, "11111111", "11111110"})
+	} else if netmask >= 16 && netmask < 24 {
+		networkOctet := -16 + netmask
+		octet := binaryNetwork[2]
+		octet = octet[:networkOctet] + strings.Repeat("1", 8-networkOctet)
+		copy(binaryNetwork[2:], []string{octet, "11111110"})
+	} else if netmask >= 24 {
+		networkOctet := -24 + netmask
+		octet := binaryNetwork[3]
+		octet = octet[:networkOctet] + strings.Repeat("1", 8-networkOctet-1) + "0"
+		copy(binaryNetwork[3:], []string{octet})
+	} // binaryNetwork = [00001010 11111111 11100000 00000000]
+
+	for _, octet := range binaryNetwork {
+		octetIP, _ := strconv.ParseInt(octet, 2, 64)
+		networkAddress = append(networkAddress, fmt.Sprintf("%d", octetIP))
+	} // networkAddress = [10 255 224 0]
+
+	maxHost := strings.Join(networkAddress, ".")
+
+	return maxHost // 10.255.255.241
+}
+
+func getBroadcast(binaryNet []string, netmask int) string {
+	var networkAddress []string
+
+	binaryNetwork := make([]string, 4)
+	copy(binaryNetwork, binaryNet)
+
+	if netmask < 8 {
+		octet := binaryNetwork[1]
+		octet = octet[:netmask] + strings.Repeat("1", 8-netmask)
+		copy(binaryNetwork[0:], []string{octet, "11111111", "11111111", "11111111"})
+	} else if netmask >= 8 && netmask < 16 {
+		networkOctet := -8 + netmask
+		octet := binaryNetwork[1]
+		octet = octet[:networkOctet] + strings.Repeat("1", 8-networkOctet)
+		copy(binaryNetwork[1:], []string{octet, "11111111", "11111111"})
+	} else if netmask >= 16 && netmask < 24 {
+		networkOctet := -16 + netmask
+		octet := binaryNetwork[2]
+		octet = octet[:networkOctet] + strings.Repeat("1", 8-networkOctet)
+		copy(binaryNetwork[2:], []string{octet, "11111111"})
+	} else if netmask >= 24 {
+		networkOctet := -24 + netmask
+		octet := binaryNetwork[3]
+		octet = octet[:networkOctet] + strings.Repeat("1", 8-networkOctet)
+		copy(binaryNetwork[3:], []string{octet})
 	}
 
 	for _, octet := range binaryNetwork {
-		ip_octet, _ := strconv.ParseInt(octet, 2, 64)
-		str := fmt.Sprintf("%d", ip_octet)
-		data = append(data, str)
+		octetIP, _ := strconv.ParseInt(octet, 2, 64)
+		networkAddress = append(networkAddress, fmt.Sprintf("%d", octetIP))
+	} // networkAddress = [10 255 224 0]
+
+	maxHost := strings.Join(networkAddress, ".")
+
+	return maxHost // 10.255.255.255
+}
+
+// In-Progress
+// Будет получать IP адрес и маску сети на проверку
+func validatorNetwork(binaryNet []string, netmask int) string {
+	if netmask < 1 || netmask > 32 {
+		fmt.Printf("Error: Вы ввели %d! Установить маску сети [1-32].\n", netmask)
+		os.Exit(0)
 	}
-	// fmt.Println("getHostMin:    ", binaryNetwork)
-	network := strings.Join(data, ".")
-	return network
+
+	binaryNetwork := make([]string, 4)
+	copy(binaryNetwork, binaryNet)
+
+	counter := 1
+
+	for _, octet := range binaryNetwork {
+		octetIP, _ := strconv.ParseInt(octet, 2, 64)
+		if counter == 1 && octetIP == 0 {
+			fmt.Printf("Error: Не верный адрес сети/хоста.\n")
+			os.Exit(0)
+		} else if octetIP < 0 || octetIP > 255 {
+			fmt.Printf("Error: Указали не верный адрес сети/хоста.\n")
+			os.Exit(0)
+		}
+		counter++
+
+	}
+
+	return ""
 }
